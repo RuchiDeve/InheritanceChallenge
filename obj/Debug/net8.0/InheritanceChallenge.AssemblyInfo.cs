5db0623d0ca327d06e4c7fca5047bdfbc0df393e
Israel Iyonsi <Israel.Iyonsi@africaprudential.com>
2024-02-27 21:28:11 +0100

    update

--- obj/Debug/net8.0/InheritanceChallenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/InheritanceChallenge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InheritanceChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f6a27243ee4c89b3ca7265deb67fca5988cdfc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("InheritanceChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InheritanceChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
